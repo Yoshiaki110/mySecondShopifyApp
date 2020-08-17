b6e0758377b60c8b66a2ef05fd78819a65a26959
Yoshiaki Ito <ito.yoshiaki@canon-its.co.jp>
2020-08-17 10:44:48 +0900

    4th commit

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,11 @@
+import { TextStyle } from '@shopify/polaris';
+
 const Index = () => (
   <div>
-    <p>(ito3)Sample app using React and Next.js(ito3)</p>
+    <p>Sample app using React and Next.js</p>
+    <TextStyle variation="positive">
+      Sample app using React and Next.js
+    </TextStyle>
   </div>
 );
 

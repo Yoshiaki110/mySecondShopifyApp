84b82bbf82eb1c1efa4e4325ac0866da071bdcf2
Yoshiaki Ito <ito.yoshiaki@canon-its.co.jp>
2020-08-17 10:48:28 +0900

    5th commit

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,11 @@
-import { TextStyle } from '@shopify/polaris';
+import { Page, TextStyle } from '@shopify/polaris';
 
 const Index = () => (
-  <div>
-    <p>Sample app using React and Next.js</p>
+  <Page>
     <TextStyle variation="positive">
       Sample app using React and Next.js
     </TextStyle>
-  </div>
+  </Page>
 );
 
 export default Index;
